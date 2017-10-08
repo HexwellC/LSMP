eaafd1435fec2a11399da6dad1fd182c98d9df23
fox.cpp <foxcpp@yandex.ru>
2017-10-08 16:42:49 +0300

    Rename template types in dispatcher.hpp
    Also improve documentation a bit.

--- src/utils/dispatcher.hpp
+++ src/utils/dispatcher.hpp
@@ -7,27 +7,27 @@
 #include "unpack_and_call.hpp"
 
 namespace lsmp {
-    template<typename InputT, typename TypeCodeT>
+    template<typename InputT, typename EventIdT>
     struct no_converter {
-        using type_code_type      = TypeCodeT;
-        using input_type          = InputT;
+        using event_id_t   = EventIdT;
+        using input_type   = InputT;
 
-        template<TypeCodeT TypeCode>
+        template<EventIdT EventId>
         struct handler {
             using args_tuple    = std::tuple<InputT>;
             using type          = std::function<void(args_tuple)>;
         };
 
-        template<TypeCodeT TypeCode>
+        template<EventIdT EventId>
         static auto convert(const input_type& input) {
             return std::make_tuple(input);
         }
     };
 
     /**
-     * (C++17) Universal object dispatcher.
+     * (C++17) Universal event dispatcher.
      *
-     * \tparam TypeCodeT    Type for identifier of input type.
+     * \tparam EventIdT     Type for event ID.
      * \tparam InputT       Source input type.
      * \tparam ConvereterT  Implements conversion from InputT to arguments for handlers.
      *                      (each input type can have different handler arguments).
@@ -45,30 +45,48 @@ namespace lsmp {
      * // Goes to second handler.
      * s.dispatch<lsmp::op::error>({ json_err, sigs });
      * ```
+     *
+     * ** HOW TO **
+     *
+     * 1. (Optional) Define converter type.
+     *    - Must define `event_id_t` and `input_type` types (same as dispatcher's `EventIdT` and `InputT`).
+     *    - Must define template struct `handler<event_id_t>` with `args_tuple` and `type` types.
+     *      - `args_tuple` - std::tuple with arguments to pass to handler for this event_id.
+     *      - `type`       - `std::function<void(args_tuple)>` but can be used to define custom handler type.
+     *    - Must define static `convert<event_id_t>` function, should accept `input_type` and return
+     *      matching `handler<event_id_t>::args_tuple`.
+     *
+     * 2. Use `dispatcher<event_id_t, input_type, converter>`.
+     *
+     * 3. See "Test case" at end of dispatcher.hpp for more advanced example.
+     *
      */
-    template<typename TypeCodeT,
+    template<typename EventIdT,
              typename InputT,
-             typename ConverterT = no_converter<InputT, TypeCodeT>>
+             typename ConverterT = no_converter<InputT, EventIdT>>
     class dispatcher {
     public:
-        template<TypeCodeT TypeCode>
-        void add_handler(typename ConverterT::template handler<TypeCode>::type handler) {
-            handlers[TypeCode].push_back(handler);
+        static_assert(std::is_same<EventIdT, ConverterT::event_id_t>::value, "Converter is not compatible.");
+        static_assert(std::is_same<InputT, ConverterT::input_type>::value, "Converter is not compatible.");
+
+        template<EventIdT EventId>
+        void add_handler(typename ConverterT::template handler<EventId>::type handler) {
+            handlers[EventId].push_back(handler);
         }
 
-        template<TypeCodeT TypeCode>
+        template<EventIdT EventId>
         void dispatch(const typename ConverterT::input_type& input) const {
-            auto handlers_it = handlers.find(TypeCode);
+            auto handlers_it = handlers.find(EventId);
             if (handlers_it == handlers.end()) return;
 
             for (const auto& any_handler : handlers_it->second) {
-                using handler_type = typename ConverterT::template handler<TypeCode>::type;
+                using handler_type = typename ConverterT::template handler<EventId>::type;
 
-                unpack_and_call(std::any_cast<handler_type>(any_handler), ConverterT::template convert<TypeCode>(input));
+                unpack_and_call(std::any_cast<handler_type>(any_handler), ConverterT::template convert<EventId>(input));
             }
         }
     private:
-        std::unordered_map<TypeCodeT, std::vector<std::any>> handlers;
+        std::unordered_map<EventIdT, std::vector<std::any>> handlers;
     };
 } // namespace lsmp
 
@@ -78,18 +96,18 @@ Test case:
 using namespace lsmp;
 
 struct string_to_integral_converter {
-    using type_code_type   = int;
+    using event_id_t   = int;
     using input_type    = std::string;
 
-    template<type_code_type TypeCode>
+    template<event_id_t EventId>
     struct handler {
-        static_assert(TypeCode == sizeof(short) || TypeCode == sizeof(int) || TypeCode == sizeof(long),
+        static_assert(EventId == sizeof(short) || EventId == sizeof(int) || EventId == sizeof(long),
                       "string_to_integral converter supports only signed short, int and long.");
     };
 
-    template<type_code_type TypeCode>
+    template<event_id_t EventId>
     static auto convert(const input_type&) {
-        static_assert(TypeCode == sizeof(short) || TypeCode == sizeof(int) || TypeCode == sizeof(long),
+        static_assert(EventId == sizeof(short) || EventId == sizeof(int) || EventId == sizeof(long),
                       "string_to_integral converter supports only signed short, int and long.");
     }
 };
